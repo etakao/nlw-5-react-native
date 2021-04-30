750a6abcec499ef9afb3af9be8fadf1480580f6e
Erick <ericktakao28@gmail.com>
2021-04-30 10:01:06 -0300

    Aula 3 finalizada ✅

---
+++ src/components/Header.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+
+import userImg from '../assets/erick.png';
+import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
+export function Header() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.greeting}>
+          Olá,
+        </Text>
+        <Text style={styles.userName}>
+          Erick
+        </Text>
+      </View>
+
+      <Image source={userImg} style={styles.image} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingVertical: 20,
+    marginTop: getStatusBarHeight()
+  },
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text
+  },
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 40
+  }
+})
